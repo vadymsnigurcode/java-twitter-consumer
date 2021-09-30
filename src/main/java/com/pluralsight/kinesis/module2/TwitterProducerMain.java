259df754988efd330ee92f3ee979ffd5b6991eb9
Vadym <vadym.snigur.code@gmail.com>
2021-09-30 15:46:14 +0300

    added twitter consumer code

---
+++ src/main/java/com/pluralsight/kinesis/module2/TwitterProducerMain.java
@@ -0,0 +1,113 @@
+package com.pluralsight.kinesis.module2;
+
+import com.amazonaws.services.kinesis.AmazonKinesis;
+import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
+import com.amazonaws.services.kinesis.model.PutRecordRequest;
+import com.amazonaws.services.kinesis.model.PutRecordResult;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+public class TwitterProducerMain {
+    public static void main(String... args) {
+        TwitterStream twitterStream = createTwitterStream();
+        //twitterStream.addListener(createListener());
+        StatusListener listener = new StatusListener() {
+            @Override
+            public void onException(Exception e) {
+
+            }
+
+            @Override
+            public void onStatus(Status status) {
+                if (status.getLang().equals("ru"))
+                    System.out.println("Lang is " + status.getLang() + " : " + status.getUser().getName() + " : " + status.getText());
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int i) {
+
+            }
+
+            @Override
+            public void onScrubGeo(long l, long l1) {
+
+            }
+
+            @Override
+            public void onStallWarning(StallWarning stallWarning) {
+
+            }
+        };
+        twitterStream.addListener(listener);
+        twitterStream.sample();
+    }
+
+    private static TwitterStream createTwitterStream() {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("o5qd58AWq2RunkDldVDSeQsAG")
+                .setOAuthConsumerSecret("sCox2VERwJqU5d53Ec6KcA5C8kLQm6uvh9q62o8YGoigtahMzw")
+                .setOAuthAccessToken("179171495-PKZCLxXb1nRPkIGiosuDDZBN2zjM8d3EDG2CmU5r")
+                .setOAuthAccessTokenSecret("Emq0L95Tv4W7oJX2dF3ccCoEdrO5xwOhAQSdE7j8dx26i");
+
+        return new TwitterStreamFactory(cb.build()).getInstance();
+    }
+
+    private static RawStreamListener createListener() {
+        return new TweetsStatusListener(createKinesisClient());
+    }
+
+    private static AmazonKinesis createKinesisClient() {
+        AmazonKinesisClientBuilder clientBuilder = AmazonKinesisClientBuilder
+                .standard();
+
+        return clientBuilder.build();
+    }
+
+    static class TweetsStatusListener implements RawStreamListener {
+
+
+        private final AmazonKinesis kinesisClient;
+
+        TweetsStatusListener(AmazonKinesis kinesisClient) {
+            this.kinesisClient = kinesisClient;
+        }
+
+        public void onMessage(String rawString) {
+
+
+            try {
+                Status status = TwitterObjectFactory.createStatus(rawString);
+                if (status.getUser() != null) {
+                    byte[] tweetsBytes = rawString.getBytes(StandardCharsets.UTF_8);
+
+                    PutRecordRequest putRecordRequest = new PutRecordRequest();
+                    putRecordRequest.setStreamName("tweets-stream");
+
+                    putRecordRequest.setPartitionKey(status.getLang());
+                    putRecordRequest.setData(ByteBuffer.wrap(tweetsBytes));
+
+//                    PutRecordResult putRecordResult = kinesisClient.putRecord(
+//                            putRecordRequest
+//                    );
+
+//                    System.out.println(status.getLang() + " -> " + putRecordResult);
+                    System.out.println(status.getLang() + " -> ");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void onException(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
